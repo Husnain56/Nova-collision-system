bcddea060d719ddcc759d7756abb26b391bfcef5
Husnain <husnainhere.2019.5@gmail.com>
2025-07-04 05:20:15 +0500

    Setup Basic Application and Entry Point.

---
+++ Nova/src/Nova.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// For use by Nova Applications
+
+#include <stdio.h>
+#include "Nova/Application.h"
+
+// ---Entry Point------------------
+#include "Nova/EntryPoint.h"
+//---------------------------------
